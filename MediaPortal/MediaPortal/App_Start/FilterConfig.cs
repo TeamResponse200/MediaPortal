1820a00c7a6215296b6fd4fedd817304ae382312
Kate <k.valentinovna@gmail.com>
2017-10-31 22:00:53 +0200

    Add Application Insights

--- MediaPortal/MediaPortal/App_Start/FilterConfig.cs
+++ MediaPortal/MediaPortal/App_Start/FilterConfig.cs
@@ -7,7 +7,7 @@ namespace MediaPortal
     {
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
-            filters.Add(new HandleErrorAttribute());
+            filters.Add(new ErrorHandler.AiHandleErrorAttribute());
         }
     }
 }
